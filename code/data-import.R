cf10395b3cf6cce78e7de27e7189b6a3fef369c0
Peter Schmiedeskamp <peter@thoughtspot.net>
2012-09-20 11:17:02 -0700

    Remove emme data processing functions as they will be rolled upstream into emme2 R package on CRAN.

--- code/data-import.R
+++ code/data-import.R
@@ -1,62 +1,23 @@
 # This file includes code to read and prepare the various data sources for estimation in R
 
 # Libraries
+# An example of loading the updated emme2 package from source; will not be needed after changes rolled upstream
+#remove.packages("emme2")
+#install.packages("/Users/peter/Developer/emme2-r/", repos = NULL, type="source")
+
 require(emme2)
-require(reshape2)
 
-# Emme databank
-databank <- "/Users/peter/Developer/trip-based-model-est/data/emmebank"
-
-
-### Emme convenience functions -- these could probably be rolled into a package or offered upstream to the emme2 package
-
-
-# Batch read a list of emme matrix names (short version), and return a merged data.frame
-# DANGER: This function is not exactly memory-efficient. Don't attempt on low-memory systems without refactoring.
-MBatchFetch <- function(databank, matrices) {
-  # Fetch the first matrix off the list
-  # (assumes first matrix is representative of the those following)
-  df <- MFFetch(databank, matrices[1])
+fetchSkims <- function () {
+  # Emme databank
+  databank <- "./data/emmebank"
   
-  for(m in matrices[-1]) {
-    tryCatch((new <- MFFetch(databank, m)
-              df <- merge(df, new, by=intersect(names(df), names(new)))),
-             finally = print(paste(m, " failed to read")))
-  }
-  return(df)
+  matrixlist <- c("aau1tm", "aau2tm", "aau3tm", "aa1tm1", "aa1tm2", "aa1tm3", "aa1tm4", "aau1cs", "aau2cs", "aau3cs", "aa1cs1", "aa1cs2", "aa1cs3", "aa1cs4", "aau1ds", "aau2ds", "aau3ds", "aa1ds1", "aa1ds2", "aa1ds3", "aa1ds4", "izdist", "izatim", "termtm", "abrdwa", "atrtwa", "aivtwa", "atwtwa", "aiwtwa", "anbdwa", "aauxwa", "axfrwa", "walkac", "afarwa", "awlkdc", "awlktc", "abkeds", "abketm", "ivtda1", "auxda1", "twtda1", "nbdda1", "xfrda1", "ivtda2", "auxda2", "twtda2", "nbdda2", "xfrda2", "ivtda2", "ivtda3", "auxda3", "twtda3", "nbdda3", "xfrda3", "ivtda4", "auxda4", "twtda4", "iwtda4", "nbdda4", "xfrda4", "farda1", "farda2", "farda3", "farda4", "brdda4", "brdda3", "brdda2", "brdda1", "aa1tt1", "aa1tt2", "aa1tt3", "aa1tt4", "au2btm", "au3btm", "ah1btm", "ah2btm", "ah3btm", "ah4btm", "au2bcs", "au3bcs", "ah1bcs", "ah2bcs", "ah3bcs", "ah4bcs", "trtda1", "trtda2", "trtda3", "trtda4")
+  
+  emme.df <- MFBatchFetch(databank, matrixlist)
+  
+  return(emme.df)
 }
 
-# Return the named matrix as a neatly-formatted dataframe
-MFFetch <- function(databank, matrixname, varlongname=NULL, valsonly=NULL) {
-  file0 <- read.file0(databank)
-  file1 <- read.file1(databank, file0)
-  mat.dir <- read.matdir(databank, file0, file1$global["mmat"])
-  mf <- read.mf(matrixname, databank, file0, file1$global["mcent"], mat.dir)
-  mf <- melt(mf,)
-  
-  if(is.null(varlongname)) {
-    # Convert this to a dataframe to avoid breaking my brain
-    dirdf <- data.frame(mat.dir$mf, stringsAsFactors=FALSE)
-    varlongname <- dirdf[dirdf$name==matrixname, 2]
-  }
-  
-  names(mf) <- c("orig", "dest", varlongname)
-  
-  if(is.null(valsonly)) {
-    mf
-  } else {
-    mf[-c(1,2)]
-  }
-}
-
-
-MFDir <- function(databank) {
-  # Boilerplate file index reading
-  file0 <- read.file0(databank)
-  file1 <- read.file1(databank, file0)
-  # Return a more useful data type and filter out null entries
-  mat.dir <- data.frame(read.matdir(databank, file0, file1$global["mmat"])$mf, stringsAsFactors=FALSE)
-  mat.dir <- mat.dir[mat.dir$name != "", ]
-
-  return(mat.dir)
+fetchZonal <- function() {
+  zonal.df <- read.csv("./data/zonaldata.csv", header=TRUE)
 }
